9f3ab70a740123b6b98c1518833516f107943dc0
Jan Nolten <j.nolten@fz-juelich.de>
2025-10-27 16:13:38 +0100

    Added schema_cubic.py

---
+++ elephant/schemas/schema_cubic.py
@@ -0,0 +1,32 @@
+from typing import (
+    Any,
+)
+from pydantic import (
+    BaseModel,
+    Field,
+    field_validator,
+)
+import neo
+from enum import Enum
+import elephant
+
+import sys
+from pathlib import Path
+
+# Add parent directory to sys.path
+sys.path.append(str(Path(__file__).resolve().parents[1]))
+import field_validator as fv
+
+class PydanticCubic(BaseModel):
+    """
+    PyDantic Class to wrap the elephant.cubic.cubic function
+    with additional type checking and json_schema by PyDantic.
+    """
+    histogramm: Any = Field(..., description="Population histogram of entire population")
+    max_interations: int = Field(100, gt=0, description="Maximum number of iterations")
+    alpha: float = Field(0.05, ge=0, le=1, description="Significance level")
+
+    @field_validator("histogramm")
+    @classmethod
+    def validate_neo_AnalogSignal(cls, v, info):
+        return fv.validate_type(v, info, allowed_types=(neo.AnalogSignal,), allow_none=False)
